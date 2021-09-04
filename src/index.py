88ee69e579c4caa11a9d047560d86ff9b917474b
gStigma <jimenezhernandezjosemanuel30@gmail.com>
2021-09-03 21:53:53 -0500

    bot by stigma

---
+++ src/index.py
@@ -0,0 +1,88 @@
+import discord 
+from discord.ext import commands
+import youtube_dl
+import os
+
+
+bot = commands.Bot(command_prefix = "|",description = "this is a bot created by stigma")
+
+@bot.command()
+async def estado(ctx): #recibe un parametro el cual es el contexto
+    await ctx.send('todo bien')
+
+@bot.command()
+async def systemError(ctx):
+    await ctx.send('discord.ext.commands.errors.CommandInvokeError: Command raised an exception: TypeError: send() got an unexpected keyword argument end')
+
+
+
+#Events
+@bot.event
+async def on_ready():
+      print("Bot establecido")
+      await bot.change_presence(activity = discord.Streaming(name="Genshin impact",url="http:/www.twitch.tv/class_system"))
+
+@bot.command()
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Admins')
+    await voiceChannel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+bot.run('ODgwOTkwMjI2ODAwMDgyOTk1.YSmUFg.cEQc4dXGPIry5DFX4IC9qozht84') #el parametro sera el token
+
